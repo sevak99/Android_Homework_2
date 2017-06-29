834e74f897213a8d949ad8f6524e07eda59a7dea
Sevak Abrahamyan <sevak_abrahamyan_99@mail.ru>
2017-06-29 11:24:08 +0400

    [*] Changed MainActivity

--- app/src/main/java/com/example/homework_2/MainActivity.java
+++ app/src/main/java/com/example/homework_2/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.homework_2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+//mainActivity
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView textView1;
